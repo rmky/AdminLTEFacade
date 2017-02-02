3b3b568b8a6799e414e3973fb61864fb3a464812
Stefan Leupold <stefan.leupold@salt-solutions.de>
2017-02-02 17:48:08 +0100

    #56308 Korrekte Anzeige aller ModalDialogs auf der Kasse
    Der modal-body-content-wrapper wurde an den restlichen Stellen im
    Code eingefuegt, damit eine konistente Anzeige erreicht wird.

--- Template/Elements/lteDataTable.php
+++ Template/Elements/lteDataTable.php
@@ -706,22 +706,24 @@ JS;
 				<h4 class="modal-title">Table settings</h4>
 			</div>
 			<div class="modal-body">
-				<div role="tabpanel">
-
-					<!-- Nav tabs -->
-					<ul class="nav nav-tabs" role="tablist">
-						<li role="presentation" class="active"><a href="#{$this->get_id()}_popup_filters" aria-controls="{$this->get_id()}_popup_filters" role="tab" data-toggle="tab">Filters</a></li>
-						<li role="presentation"><a href="#{$this->get_id()}_popup_columns" aria-controls="{$this->get_id()}_popup_columns" role="tab" data-toggle="tab">Columns</a></li>
-						<li role="presentation"><a href="#{$this->get_id()}_popup_sorting" aria-controls="{$this->get_id()}_popup_sorting" role="tab" data-toggle="tab">Sorting</a></li>
-					</ul>
-									
-					<!-- Tab panes -->
-					<div class="tab-content">
-						<div role="tabpanel" class="tab-pane active" id="{$this->get_id()}_popup_filters">{$filters_html}</div>
-						<div role="tabpanel" class="tab-pane" id="{$this->get_id()}_popup_columns">{$columns_html}</div>
-						<div role="tabpanel" class="tab-pane" id="{$this->get_id()}_popup_sorting">{$sorting_html}</div>
+				<div class="modal-body-content-wrapper">
+					<div role="tabpanel">
+	
+						<!-- Nav tabs -->
+						<ul class="nav nav-tabs" role="tablist">
+							<li role="presentation" class="active"><a href="#{$this->get_id()}_popup_filters" aria-controls="{$this->get_id()}_popup_filters" role="tab" data-toggle="tab">Filters</a></li>
+							<li role="presentation"><a href="#{$this->get_id()}_popup_columns" aria-controls="{$this->get_id()}_popup_columns" role="tab" data-toggle="tab">Columns</a></li>
+							<li role="presentation"><a href="#{$this->get_id()}_popup_sorting" aria-controls="{$this->get_id()}_popup_sorting" role="tab" data-toggle="tab">Sorting</a></li>
+						</ul>
+										
+						<!-- Tab panes -->
+						<div class="tab-content">
+							<div role="tabpanel" class="tab-pane active" id="{$this->get_id()}_popup_filters">{$filters_html}</div>
+							<div role="tabpanel" class="tab-pane" id="{$this->get_id()}_popup_columns">{$columns_html}</div>
+							<div role="tabpanel" class="tab-pane" id="{$this->get_id()}_popup_sorting">{$sorting_html}</div>
+						</div>
+						
 					</div>
-					
 				</div>
 			</div>
 			<div class="modal-footer">
