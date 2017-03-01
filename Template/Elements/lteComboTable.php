8e3d4370268908a0ad2b0e4abefee5586fd7c518
Stefan Leupold <stefan.leupold@salt-solutions.de>
2017-03-01 10:05:38 +0100

    #56308 Anpassungen an ComboTable fuer Live-References

--- Template/Elements/lteComboTable.php
+++ Template/Elements/lteComboTable.php
@@ -30,71 +30,34 @@ class lteComboTable extends lteInput {
 		$widget = $this->get_widget();
 		
 		// Add other options
-		$other_options = (!$widget->get_multi_select() ? ',maxSelection: 1' : '') . ($widget->is_disabled() ? ',disabled: true' : '');
-		
-		// Set initial value
-		$filters = [];
-		if ($widget->get_value_expression() && $widget->get_value_expression()->is_reference()){
-			//widget has a live reference value
-			$link = $widget->get_value_expression()->get_widget_link();
-			$linked_element = $this->get_template()->get_element_by_widget_id($link->get_widget_id(), $this->get_page_id());
-			if ($widget->get_value_text()){
-				$initial_value_script = 'ms.setSelection([{"' . $widget->get_text_column()->get_data_column_name() . '": "' . $widget->get_value_text() . '", "' . $widget->get_value_column()->get_data_column_name() . '": ' . $linked_element->build_js_value_getter() . '}]);';
-			} else {
-				//$initial_value_script = 'ms.setValue([' . $linked_element->build_js_value_getter() . '])';
-				$filters[] = 'fltr' . str_pad(count($filters), 2, 0, STR_PAD_LEFT) . '_' . $widget->get_value_column()->get_data_column_name() . ': ' . $linked_element->build_js_value_getter();
-			}
-		} else {
-			//widget has a static value
-			if ($widget->get_value_text()){
-				$initial_value_script = 'ms.setSelection([{"' . $widget->get_text_column()->get_data_column_name() . '": "' . $widget->get_value_text() . '", "' . $widget->get_value_column()->get_data_column_name() . '": "' . $widget->get_value() . '"}]);';
-			} else {
-				//$initial_value_script = 'ms.setValue([' . $widget->get_value() . '])';
-				$filters[] = 'fltr' . str_pad(count($filters), 2, 0, STR_PAD_LEFT) . '_' . $widget->get_value_column()->get_data_column_name() . ': "' . $widget->get_value() . '"';
-			}
-		}
-		
-		// Add initial filters
-		if ($widget->get_table()->has_filters()){
-			foreach ($widget->get_table()->get_filters() as $fltr){
-				if ($fltr->get_value_expression() && $fltr->get_value_expression()->is_reference()){
-					//filter is a live reference
-					$link = $fltr->get_value_expression()->get_widget_link();
-					$linked_element = $this->get_template()->get_element_by_widget_id($link->get_widget_id(), $this->get_page_id());
-					$filters[] = 'fltr' . str_pad(count($filters), 2, 0, STR_PAD_LEFT) . '_' . $fltr->get_attribute_alias() . ': ' . $linked_element->build_js_value_getter($link->get_column_id());
-				} else {
-					//filter has a static value
-					$filters[] = 'fltr' . str_pad(count($filters), 2, 0, STR_PAD_LEFT) . '_' . $fltr->get_attribute_alias() . ': "' . $fltr->get_value() . '"'; 
-				}
-			}
-		}
-		$initial_value_filter = implode(",\n\t\t\t", $filters);
+		$options = [];
+		if (!$widget->get_multi_select()) {$options[] = 'maxSelection: 1'; }
+		if ($widget->is_disabled()) { $options[] = 'disabled: true'; }
+		$other_options = implode(",\n\t\t", $options);
+		$other_options = $other_options ? ', ' . $other_options : '';
 		
 		$output = <<<JS
 		
 $(document).ready(function() {
 	
-    var ms = $('#{$this->get_id()}_ms').magicSuggest({
-    	data: "{$this->get_ajax_url()}",
-        dataUrlParams: {
-        	resource: "{$this->get_page_id()}",
+	var ms = $('#{$this->get_id()}_ms').magicSuggest({
+		data: "{$this->get_ajax_url()}",
+		dataUrlParams: {
+			resource: "{$this->get_page_id()}",
 			element: "{$widget->get_table()->get_id()}",
 			object: "{$widget->get_table()->get_meta_object()->get_id()}",
 			action: "{$widget->get_lazy_loading_action()}",
 			length: {$widget->get_max_suggestions()},
 			start: 0,
-			{$initial_value_filter}
-        },
-        queryParam: 'q',
-        resultsField: 'data',
-        valueField: '{$widget->get_value_column()->get_data_column_name()}',
-        displayField: '{$widget->get_text_column()->get_data_column_name()}',
-        allowFreeEntries: false
-        ,firstLoad: true
-        {$other_options}
-    });
-	
-	{$initial_value_script}
+			initialLoad: true
+		},
+		queryParam: 'q',
+		resultsField: 'data',
+		valueField: '{$widget->get_value_column()->get_data_column_name()}',
+		displayField: '{$widget->get_text_column()->get_data_column_name()}',
+		allowFreeEntries: false
+		{$other_options}
+	});
 	
 	$(ms).on("selectionchange", function(e,m){
 		$("#{$this->get_id()}").val(m.getValue()).trigger("change");
@@ -105,7 +68,7 @@ $(document).ready(function() {
 		{$this->build_js_on_beforeload_live_reference()}
 	});
 	
-	$(ms).on("load", function(e,m){	
+	$(ms).on("load", function(e,m){
 		{$this->build_js_on_load_live_reference()}
 	});
 });		
@@ -193,22 +156,36 @@ JS;
 	function build_js_on_beforeload_live_reference() {
 		$widget = $this->get_widget();
 		
-		$output = '';
+		$fltrId = 0;
+		// Add filters from widget
+		$filters = [];
 		if ($widget->get_table()->has_filters()){
-			foreach ($widget->get_table()->get_filters() as $fnr => $fltr){
+			foreach ($widget->get_table()->get_filters() as $fltr){
 				if ($fltr->get_value_expression() && $fltr->get_value_expression()->is_reference()){
 					//filter is a live reference
 					$link = $fltr->get_value_expression()->get_widget_link();
 					$linked_element = $this->get_template()->get_element_by_widget_id($link->get_widget_id(), $this->get_page_id());
-					$output .= '
-				m.getDataUrlParams().fltr' . str_pad($fnr, 2, 0, STR_PAD_LEFT) . '_' . $fltr->get_attribute_alias() . ' = ' . $linked_element->build_js_value_getter($link->get_column_id()) . ';';
+					$filters[] = 'dataUrlParams.fltr' . str_pad($fltrId++, 2, 0, STR_PAD_LEFT) . '_' . $fltr->get_attribute_alias() . ' = ' . $linked_element->build_js_value_getter($link->get_column_id()) . ';';
 				} else {
 					//filter has a static value
-					$output .= '
-				m.getDataUrlParams().fltr' . str_pad($fnr, 2, 0, STR_PAD_LEFT) . '_' . $fltr->get_attribute_alias() . ' = "' . $fltr->get_value() . '";';
+					$filters[] = 'dataUrlParams.fltr' . str_pad($fltrId++, 2, 0, STR_PAD_LEFT) . '_' . $fltr->get_attribute_alias() . ' = "' . $fltr->get_value() . '";';
 				}
 			}
 		}
+		$filters_script = implode("\n\t\t\t\t\t", $filters);
+		// Add value filter (to show proper label for a set value)
+		$value_filters = [];
+		$value_filters[] = 'dataUrlParams.fltr' . str_pad($fltrId++, 2, 0, STR_PAD_LEFT) . '_' . $widget->get_value_column()->get_data_column_name() . ' = m.getValue().join();';
+		$value_filters_script = implode("\n\t\t\t\t\t", $value_filters);
+		
+		$output = '
+				var dataUrlParams = m.getDataUrlParams();
+				
+				if (dataUrlParams.jsValueSetterUpdate) {
+					' . $value_filters_script . '
+				} else {
+					' . $filters_script . '
+				}';
 		
 		return $output;
 	}
@@ -225,20 +202,47 @@ JS;
 	function build_js_on_load_live_reference() {
 		$widget = $this->get_widget();
 		
+		// Add initial value
+		if ($widget->get_value_expression() && $widget->get_value_expression()->is_reference()){
+			//widget has a live reference value
+			$link = $widget->get_value_expression()->get_widget_link();
+			$linked_element = $this->get_template()->get_element_by_widget_id($link->get_widget_id(), $this->get_page_id());
+			if ($widget->get_value_text()){
+				$initial_value_script= 'm.setSelection([{"' . $widget->get_text_column()->get_data_column_name() . '": "' . $widget->get_value_text() . '", "' . $widget->get_value_column()->get_data_column_name() . '": ' . $linked_element->build_js_value_getter($link->get_column_id()) . '}]);';
+			} else {
+				$initial_value_script = 'm.setValue([' . $linked_element->build_js_value_getter($link->get_column_id()) . '])';
+			}
+		} elseif ($widget->get_value()) {
+			//widget has a static value
+			if ($widget->get_value_text()){
+				$initial_value_script = 'm.setSelection([{"' . $widget->get_text_column()->get_data_column_name() . '": "' . $widget->get_value_text() . '", "' . $widget->get_value_column()->get_data_column_name() . '": "' . $widget->get_value() . '"}]);';
+			} else {
+				$initial_value_script = 'm.setValue([' . $widget->get_value() . '])';
+			}
+		}
+		
 		$output = '
-				if (m.getDataUrlParams().jsValueSetterUpdate) {
-					// Nach dem Laden wird der Wert neu gesetzt um ordentliche Label anzuzeigen
+				var dataUrlParams = m.getDataUrlParams();
+				
+				for (key in dataUrlParams) {
+					if (key.substring(0, 4) == "fltr") {
+						delete dataUrlParams[key];
+					}
+				}
+				
+				if (dataUrlParams.jsValueSetterUpdate) {
 					var value = m.getValue();
 					m.clear();
 					m.setValue(value);
 					
-					// dataUrlParams aufraeumen
-					delete m.getDataUrlParams().jsValueSetterUpdate;
-					for (key in m.getDataUrlParams()) {
-						if (key.substring(0, 4) == "fltr") {
-							delete m.getDataUrlParams()[key];
-						}
-					}
+					delete dataUrlParams.jsValueSetterUpdate;
+				}
+				
+				if (dataUrlParams.initialLoad) {
+					' . $initial_value_script . '
+					delete dataUrlParams.initialLoad;
+					dataUrlParams.jsValueSetterUpdate = true;
+					m.setData("' . $this->get_ajax_url() . '");
 				}';
 		
 		return $output;
