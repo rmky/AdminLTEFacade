42f664a995739c66bfa32141a0b87c98383c446e
stevi84 <stevi84@gmx.de>
2017-03-01 15:40:35 +0100

    Issue 56308 (#9)
    * #56308 Toast-Notifications fuer AdminLte
    
    * #56308 Anpassungen an ComboTable fuer Live-References
    
    * #56308 css-Styles fuer Toast-Notifications
    
    * #56308 Anpassungen an ComboTable fuer Live-References
    
    * #56308 Doppeltes Laden der ComboTables zurueckgedreht

--- Template/Elements/lteComboTable.php
+++ Template/Elements/lteComboTable.php
@@ -29,76 +29,62 @@ class lteComboTable extends lteInput {
 		/* @var $widget \exface\Core\Widgets\ComboTable */
 		$widget = $this->get_widget();
 		
-		// Add other options
-		$other_options = (!$widget->get_multi_select() ? ',maxSelection: 1' : '') . ($widget->is_disabled() ? ',disabled: true' : '');
-		
-		// Set initial value
-		$filters = [];
+		// Add initial value
 		if ($widget->get_value_expression() && $widget->get_value_expression()->is_reference()){
 			//widget has a live reference value
 			$link = $widget->get_value_expression()->get_widget_link();
 			$linked_element = $this->get_template()->get_element_by_widget_id($link->get_widget_id(), $this->get_page_id());
 			if ($widget->get_value_text()){
-				$initial_value_script = 'ms.setSelection([{"' . $widget->get_text_column()->get_data_column_name() . '": "' . $widget->get_value_text() . '", "' . $widget->get_value_column()->get_data_column_name() . '": ' . $linked_element->build_js_value_getter() . '}]);';
+				$initial_value_script= 'ms.setSelection([{"' . $widget->get_text_column()->get_data_column_name() . '": "' . $widget->get_value_text() . '", "' . $widget->get_value_column()->get_data_column_name() . '": ' . $linked_element->build_js_value_getter($link->get_column_id()) . '}]);';
 			} else {
-				//$initial_value_script = 'ms.setValue([' . $linked_element->build_js_value_getter() . '])';
-				$filters[] = 'fltr' . str_pad(count($filters), 2, 0, STR_PAD_LEFT) . '_' . $widget->get_value_column()->get_data_column_name() . ': ' . $linked_element->build_js_value_getter();
+				$initial_value_script = 'ms.setValue([' . $linked_element->build_js_value_getter($link->get_column_id()) . '])';
+				$initial_filter_script = ', fltr00_' . $widget->get_value_column()->get_data_column_name() . ': ' . $linked_element->build_js_value_getter($link->get_column_id());
 			}
 		} elseif ($widget->get_value()) {
 			//widget has a static value
 			if ($widget->get_value_text()){
 				$initial_value_script = 'ms.setSelection([{"' . $widget->get_text_column()->get_data_column_name() . '": "' . $widget->get_value_text() . '", "' . $widget->get_value_column()->get_data_column_name() . '": "' . $widget->get_value() . '"}]);';
 			} else {
-				//$initial_value_script = 'ms.setValue([' . $widget->get_value() . '])';
-				$filters[] = 'fltr' . str_pad(count($filters), 2, 0, STR_PAD_LEFT) . '_' . $widget->get_value_column()->get_data_column_name() . ': "' . $widget->get_value() . '"';
+				$initial_value_script = 'ms.setValue([' . $widget->get_value() . '])';
+				$initial_filter_script = ', fltr00_' . $widget->get_value_column()->get_data_column_name() . ': ' . $widget->get_value();
 			}
 		}
 		
-		// Add initial filters
-		if ($widget->get_table()->has_filters()){
-			foreach ($widget->get_table()->get_filters() as $fltr){
-				if ($fltr->get_value_expression() && $fltr->get_value_expression()->is_reference()){
-					//filter is a live reference
-					$link = $fltr->get_value_expression()->get_widget_link();
-					$linked_element = $this->get_template()->get_element_by_widget_id($link->get_widget_id(), $this->get_page_id());
-					$filters[] = 'fltr' . str_pad(count($filters), 2, 0, STR_PAD_LEFT) . '_' . $fltr->get_attribute_alias() . ': ' . $linked_element->build_js_value_getter($link->get_column_id());
-				} else {
-					//filter has a static value
-					$filters[] = 'fltr' . str_pad(count($filters), 2, 0, STR_PAD_LEFT) . '_' . $fltr->get_attribute_alias() . ': "' . $fltr->get_value() . '"'; 
-				}
-			}
-		}
-		$initial_value_filter = implode(",\n\t\t\t", $filters);
-		$initial_value_filter = $initial_value_filter ? ', '.  $initial_value_filter : '';
+		// Add other options
+		$options = [];
+		if (!$widget->get_multi_select()) {$options[] = 'maxSelection: 1'; }
+		if ($widget->is_disabled()) { $options[] = 'disabled: true'; }
+		$other_options = implode(",\n\t\t", $options);
+		$other_options = $other_options ? ', ' . $other_options : '';
+		
 		
 		$output = <<<JS
 		
 $(document).ready(function() {
 	
-    var ms = $('#{$this->get_id()}_ms').magicSuggest({
-    	data: "{$this->get_ajax_url()}",
-        dataUrlParams: {
-        	resource: "{$this->get_page_id()}",
+	var ms = $('#{$this->get_id()}_ms').magicSuggest({
+		data: "{$this->get_ajax_url()}",
+		dataUrlParams: {
+			resource: "{$this->get_page_id()}",
 			element: "{$widget->get_table()->get_id()}",
 			object: "{$widget->get_table()->get_meta_object()->get_id()}",
 			action: "{$widget->get_lazy_loading_action()}",
 			length: {$widget->get_max_suggestions()},
 			start: 0
-			{$initial_value_filter}
-        },
-        queryParam: 'q',
-        resultsField: 'data',
-        valueField: '{$widget->get_value_column()->get_data_column_name()}',
-        displayField: '{$widget->get_text_column()->get_data_column_name()}',
-        allowFreeEntries: false
-        ,firstLoad: true
-        {$other_options}
-    });
+			{$initial_filter_script}
+		},
+		queryParam: 'q',
+		resultsField: 'data',
+		valueField: '{$widget->get_value_column()->get_data_column_name()}',
+		displayField: '{$widget->get_text_column()->get_data_column_name()}',
+		allowFreeEntries: false
+		{$other_options}
+	});
 	
 	{$initial_value_script}
 	
 	$(ms).on("selectionchange", function(e,m){
-		$("#{$this->get_id()}").val(m.getValue()).trigger("change");
+		$("#{$this->get_id()}").val(m.getValue().join()).trigger("change");
 		{$this->get_on_change_script()}
 	});
 	
@@ -106,7 +92,7 @@ $(document).ready(function() {
 		{$this->build_js_on_beforeload_live_reference()}
 	});
 	
-	$(ms).on("load", function(e,m){	
+	$(ms).on("load", function(e,m){
 		{$this->build_js_on_load_live_reference()}
 	});
 });		
@@ -131,17 +117,31 @@ JS;
 	 * Werte sind per MagicSuggest valueField definiert. Sind mehrere Werte ausgewaehlt
 	 * wird eine Komma-separierte Liste dieser Werte zurueckgegeben. Ist eine spezifische
 	 * Spalte ausgewaehlt, wird statt dem valueField der Wert dieser Spalte zurueckgegeben.
+	 * Ist MagicSuggest noch nicht erzeugt wird stattdessen der Wert aus dem verknuepften
+	 * InputHidden zurueckgegeben.
 	 * 
 	 * {@inheritDoc}
 	 * @see \exface\AbstractAjaxTemplate\Template\Elements\AbstractJqueryElement::build_js_value_getter()
 	 */
 	function build_js_value_getter($column = null, $row = null){
 		if ($this->get_widget()->get_multi_select() || is_null($column) || $column === ''){
-			$output = '$("#' . $this->get_id() . '_ms").magicSuggest().getValue().join()';
+			$output = '(function() {
+					var ' . $this->get_id() . '_ms = $("#' . $this->get_id() . '_ms");
+					if (' . $this->get_id() . '_ms.data("magicSuggest")) {
+						return ' . $this->get_id() . '_ms.magicSuggest().getValue().join();
+					} else {
+						return $("#' . $this->get_id() . '").val();
+					}
+				})()';
 		} else {
 			$output = '(function() {
-					var row = $("#' . $this->get_id() . '_ms").magicSuggest().getSelection();
-					if (row.length > 0) { return row[0]["' . $column . '"]; } else { return ""; }
+					var ' . $this->get_id() . '_ms = $("#' . $this->get_id() . '_ms");
+					if (' . $this->get_id() . '_ms.data("magicSuggest")) {
+						var row = ' . $this->get_id() . '_ms.magicSuggest().getSelection();
+						if (row.length > 0) { return row[0]["' . $column . '"]; } else { return ""; }
+					} else {
+						return $("#' . $this->get_id() . '").val();
+					}
 				})()';
 		}
 		
@@ -152,7 +152,8 @@ JS;
 	 * Erzeugung einer JavaScript-Funktion zum Setzen des Wertes. Ist multiselect false
 	 * wird der Wert nur gesetzt wenn genau ein Wert uebergeben wird. Anschliessend wird
 	 * der Inhalt des MagicSuggest neu geladen (um ordentliche Label anzuzeigen falls
-	 * auch ein entsprechender Filter gesetzt ist).
+	 * auch ein entsprechender Filter gesetzt ist). Ist MagicSuggest noch nicht erzeugt
+	 * wird stattdessen der Wert im verknuepften InputHidden gesetzt.
 	 * 
 	 * {@inheritDoc}
 	 * @see \exface\AdminLteTemplate\Template\Elements\lteInput::build_js_value_setter()
@@ -161,85 +162,106 @@ JS;
 		$widget = $this->get_widget();
 		
 		$output = '
-				var ' . $this->get_id() . '_ms = $("#' . $this->get_id() . '_ms").magicSuggest();
+				var ' . $this->get_id() . '_ms = $("#' . $this->get_id() . '_ms");
 				var value = ' . $value . ', valueArray;
-				if (value) { valueArray = $.map(value.split(","), $.trim); } else { valueArray = []; }
-				' . $this->get_id() . '_ms.clear();';
+				if (' . $this->get_id() . '_ms.data("magicSuggest")) {
+					if (value) { valueArray = $.map(value.split(","), $.trim); } else { valueArray = []; }
+					' . $this->get_id() . '_ms.magicSuggest().clear();';
 		
 		if ($this->get_widget()->get_multi_select()) {
 			$output .= '
-				' . $this->get_id() . '_ms.setValue(valueArray);';
+					' . $this->get_id() . '_ms.magicSuggest().setValue(valueArray);
+					$("#' . $this->get_id() . '").val(value).trigger("change");';
 		} else {
 			$output .= '
-				if (valueArray.length == 1) {
-					' . $this->get_id() . '_ms.setValue(valueArray);
-				}';
+					if (valueArray.length == 1) {
+						' . $this->get_id() . '_ms.magicSuggest().setValue(valueArray);
+						$("#' . $this->get_id() . '").val(value).trigger("change");
+					}';
 		}
 		
 		$output .= '
-				' . $this->get_id() . '_ms.getDataUrlParams().jsValueSetterUpdate = true;
-				' . $this->get_id() . '_ms.setData("' . $this->get_ajax_url() . '");';
+					' . $this->get_id() . '_ms.magicSuggest().getDataUrlParams().jsValueSetterUpdate = true;
+					' . $this->get_id() . '_ms.magicSuggest().setData("' . $this->get_ajax_url() . '");
+				} else {
+					$("#' . $this->get_id() . '").val(value).trigger("change");
+				}';
 		
 		return $output;
 	}
 	
 	/**
 	 * Erzeugt den JavaScript-Code welcher vor dem Laden des MagicSuggest-Inhalts
-	 * ausgefuehrt wird. Es werden die gesetzten Filter den dataUrlParams hinzuge-
-	 * fuegt (werden nach dem Laden wieder entfernt, da sich die Werte durch Live-
-	 * Referenzen aendern koennen).
+	 * ausgefuehrt wird. Wurde programmatisch ein Wert gesetzt, wird als Filter
+	 * nur dieser Wert hinzugefuegt, um das Label ordentlich anzuzeigen. Sonst werden
+	 * die am Widget definierten Filter gesetzt. Die Filter werden nach dem Laden
+	 * wieder entfernt, da sich die Werte durch Live-Referenzen aendern koennen.
 	 * 
 	 * @return string
 	 */
 	function build_js_on_beforeload_live_reference() {
 		$widget = $this->get_widget();
 		
-		$output = '';
+		$fltrId = 0;
+		// Add filters from widget
+		$filters = [];
 		if ($widget->get_table()->has_filters()){
-			foreach ($widget->get_table()->get_filters() as $fnr => $fltr){
+			foreach ($widget->get_table()->get_filters() as $fltr){
 				if ($fltr->get_value_expression() && $fltr->get_value_expression()->is_reference()){
 					//filter is a live reference
 					$link = $fltr->get_value_expression()->get_widget_link();
 					$linked_element = $this->get_template()->get_element_by_widget_id($link->get_widget_id(), $this->get_page_id());
-					$output .= '
-				m.getDataUrlParams().fltr' . str_pad($fnr, 2, 0, STR_PAD_LEFT) . '_' . $fltr->get_attribute_alias() . ' = ' . $linked_element->build_js_value_getter($link->get_column_id()) . ';';
+					$filters[] = 'dataUrlParams.fltr' . str_pad($fltrId++, 2, 0, STR_PAD_LEFT) . '_' . $fltr->get_attribute_alias() . ' = ' . $linked_element->build_js_value_getter($link->get_column_id()) . ';';
 				} else {
 					//filter has a static value
-					$output .= '
-				m.getDataUrlParams().fltr' . str_pad($fnr, 2, 0, STR_PAD_LEFT) . '_' . $fltr->get_attribute_alias() . ' = "' . $fltr->get_value() . '";';
+					$filters[] = 'dataUrlParams.fltr' . str_pad($fltrId++, 2, 0, STR_PAD_LEFT) . '_' . $fltr->get_attribute_alias() . ' = "' . $fltr->get_value() . '";';
 				}
 			}
 		}
+		$filters_script = implode("\n\t\t\t\t\t", $filters);
+		// Add value filter (to show proper label for a set value)
+		$value_filters = [];
+		$value_filters[] = 'dataUrlParams.fltr' . str_pad($fltrId++, 2, 0, STR_PAD_LEFT) . '_' . $widget->get_value_column()->get_data_column_name() . ' = m.getValue().join();';
+		$value_filters_script = implode("\n\t\t\t\t\t", $value_filters);
+		
+		$output = '
+				var dataUrlParams = m.getDataUrlParams();
+				
+				if (dataUrlParams.jsValueSetterUpdate) {
+					' . $value_filters_script . '
+				} else {
+					' . $filters_script . '
+				}';
 		
 		return $output;
 	}
 	
 	/**
 	 * Erzeugt den JavaScript-Code welcher nach dem Laden des MagicSuggest-Inhalts
-	 * ausgefuehrt wird. Der Wert wird neu gesetzt um das Label ordentlich anzu-
-	 * zeigen. Ausserdem werden gesetzten Filter nach dem Laden wieder entfernt,
-	 * da sich die Werte durch Live-Referenzen aendern koennen (werden vor dem
-	 * naechsten Laden wieder hinzugefuegt).
+	 * ausgefuehrt wird. Alle gesetzten Filter werden entfernt, da sich die Werte
+	 * durch Live-Referenzen aendern koennen (werden vor dem naechsten Laden wieder
+	 * hinzugefuegt). Wurde der Wert zuvor programmatisch gesetzt, wird er neu
+	 * gesetzt um das Label ordentlich anzuzeigen. Nach der Erzeugung von MagicSuggest
+	 * werden initiale Werte gesetzt und neu geladen.
 	 * 
 	 * @return string
 	 */
 	function build_js_on_load_live_reference() {
-		$widget = $this->get_widget();
-		
 		$output = '
-				if (m.getDataUrlParams().jsValueSetterUpdate) {
-					// Nach dem Laden wird der Wert neu gesetzt um ordentliche Label anzuzeigen
+				var dataUrlParams = m.getDataUrlParams();
+				
+				for (key in dataUrlParams) {
+					if (key.substring(0, 4) == "fltr") {
+						delete dataUrlParams[key];
+					}
+				}
+				
+				if (dataUrlParams.jsValueSetterUpdate) {
 					var value = m.getValue();
 					m.clear();
 					m.setValue(value);
 					
-					// dataUrlParams aufraeumen
-					delete m.getDataUrlParams().jsValueSetterUpdate;
-					for (key in m.getDataUrlParams()) {
-						if (key.substring(0, 4) == "fltr") {
-							delete m.getDataUrlParams()[key];
-						}
-					}
+					delete dataUrlParams.jsValueSetterUpdate;
 				}';
 		
 		return $output;
