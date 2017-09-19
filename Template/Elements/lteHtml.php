4a89dea11f1f26b73c3c5df8dc5ee2dcb7adaf91
aka <andrej.kabachnik@salt-solutions.de>
2017-09-19 17:03:18 +0200

    REFACTOR use JqueryHtmlTrait

--- Template/Elements/lteHtml.php
+++ Template/Elements/lteHtml.php
@@ -1,29 +1,13 @@
 <?php
 namespace exface\AdminLteTemplate\Template\Elements;
 
+use exface\Core\Templates\AbstractAjaxTemplate\Elements\JqueryHtmlTrait;
+
 class lteHtml extends lteText
 {
+    use JqueryHtmlTrait;
 
     function init()
     {}
-
-    function generateHtml()
-    {
-        $output = '';
-        if ($this->getWidget()->getCss()) {
-            $output .= '<style>' . $this->getWidget()->getCss() . '</style>';
-        }
-        if ($this->getWidget()->getCaption() && ! $this->getWidget()->getHideCaption()) {
-            $output .= '<label for="' . $this->getId() . '">' . $this->getWidget()->getCaption() . '</label>';
-        }
-        
-        $output .= '<div id="' . $this->getId() . '">' . $this->getWidget()->getHtml() . '</div>';
-        return $this->buildHtmlWrapper($output);
-    }
-
-    function generateJs()
-    {
-        return $this->getWidget()->getJavascript();
-    }
 }
 ?>
