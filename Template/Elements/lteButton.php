b12d19779b54b99a0fe10e55043ca74be384e6a9
Stefan Leupold <stefan.leupold@salt-solutions.de>
2017-10-18 17:24:23 +0200

    FIX Anpassungen UiPageInstaller

--- Template/Elements/lteButton.php
+++ Template/Elements/lteButton.php
@@ -85,7 +85,7 @@ class lteButton extends lteAbstractElement
         /* @var $prefill_link \exface\Core\CommonLogic\WidgetLink */
         $prefill = '';
         if ($prefill_link = $this->getAction()->getPrefillWithDataFromWidgetLink()) {
-            if ($prefill_link->getPageAlias() == $widget->getPageAlias()) {
+            if ($prefill_link->getPage()->getAliasWithNamespace() == $this->getPageAlias()) {
                 $prefill = ", prefill: " . $this->getTemplate()->getElement($prefill_link->getWidget())->buildJsDataGetter($this->getAction());
             }
         }
@@ -100,7 +100,7 @@ class lteButton extends lteAbstractElement
 							dataType: 'html',
 							data: {
 								action: '{$widget->getActionAlias()}',
-								resource: '{$widget->getPageAlias()}',
+								resource: '{$this->getPageAlias()}',
 								element: '{$widget->getId()}',
 								data: requestData
 								{$prefill}
