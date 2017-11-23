6299f0b582899924a2fcf81e2286b5878e3f6db2
Andrej Kabachnik <andrej_ka@gmx.de>
2017-11-23 07:23:36 +0100

    REFACTOR use HtmlImageTrait

--- Template/Elements/lteImage.php
+++ Template/Elements/lteImage.php
@@ -2,6 +2,7 @@
 namespace exface\AdminLteTemplate\Template\Elements;
 
 use exface\Core\Widgets\Image;
+use exface\Core\Templates\AbstractAjaxTemplate\Elements\HtmlImageTrait;
 
 /**
  *
@@ -12,32 +13,11 @@ use exface\Core\Widgets\Image;
  */
 class lteImage extends lteText
 {
-
-    function generateHtml()
+    use HtmlImageTrait;
+    
+    protected function buildCssElementClass()
     {
-        $style = '';
-        if (! $this->getWidget()->getWidth()->isUndefined()) {
-            $width = ' width="' . $this->getWidth() . '"';
-        }
-        if (! $this->getWidget()->getHeight()->isUndefined()) {
-            $height = ' height="' . $this->getHeight() . '"';
-        }
-        
-        switch ($this->getWidget()->getAlign()) {
-            case EXF_ALIGN_CENTER:
-                $style .= 'margin-left: auto; margin-right: auto;';
-                break;
-            case EXF_ALIGN_RIGHT:
-                $style .= 'float: right';
-        }
-        
-        $output = '<img src="' . $this->getWidget()->getUri() . '"' . $width . $height . ' class="img-responsive" style="' . $style . '" />';
-        return $output;
-    }
-
-    function generateJs()
-    {
-        return '';
+        return 'img-responsive';
     }
 }
 ?>
