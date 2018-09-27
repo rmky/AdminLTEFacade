73b16597ed6fc53a885cfb237f3969b79580c092
aka <andrej.kabachnik@salt-solutions.de>
2018-09-27 11:28:20 +0200

    NEW configuration for PWA and ServiceWorker installer

--- AdminLteTemplateApp.php
+++ AdminLteTemplateApp.php
@@ -5,6 +5,8 @@ use exface\Core\Interfaces\InstallerInterface;
 use exface\Core\Templates\AbstractHttpTemplate\HttpTemplateInstaller;
 use exface\Core\CommonLogic\Model\App;
 use exface\Core\Factories\TemplateFactory;
+use exface\Core\Templates\AbstractPWATemplate\ServiceWorkerInstaller;
+use exface\Core\Templates\AbstractPWATemplate\ServiceWorkerBuilder;
 
 class AdminLteTemplateApp extends App
 {
@@ -19,9 +21,32 @@ class AdminLteTemplateApp extends App
     public function getInstaller(InstallerInterface $injected_installer = null)
     {
         $installer = parent::getInstaller($injected_installer);
+        
+        // Routing installer
         $tplInstaller = new HttpTemplateInstaller($this->getSelector());
         $tplInstaller->setTemplate(TemplateFactory::createFromString('exface.AdminLteTemplate.AdminLteTemplate', $this->getWorkbench()));
         $installer->addInstaller($tplInstaller);
+        
+        // ServiceWorker installer
+        $serviceWorkerBuilder = new ServiceWorkerBuilder();
+        foreach ($this->getConfig()->getOption('INSTALLER.SERVICEWORKER.ROUTES') as $id => $uxon) {
+            $serviceWorkerBuilder->addRouteToCache(
+                $id,
+                $uxon->getProperty('matcher'),
+                $uxon->getProperty('strategy'),
+                $uxon->getProperty('method'),
+                $uxon->getProperty('description'),
+                $uxon->getProperty('cacheName'),
+                $uxon->getProperty('maxEntries'),
+                $uxon->getProperty('maxAgeSeconds')
+                );
+        }
+        foreach ($this->getConfig()->getOption('INSTALLER.SERVICEWORKER.IMPORTS') as $path) {
+            $serviceWorkerBuilder->addImport($this->getWorkbench()->getCMS()->buildUrlToInclude($path));
+        }
+        $serviceWorkerInstaller = new ServiceWorkerInstaller($this->getSelector(), $serviceWorkerBuilder);
+        $installer->addInstaller($serviceWorkerInstaller);
+        
         return $installer;
     }
 }
