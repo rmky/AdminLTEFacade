ea79aa22de9861fed40199f7a6cd412321a79fb1
Andrej Kabachnik <andrej_ka@gmx.de>
2019-02-25 08:33:13 +0100

    FIX errors in buttons with prefill links

--- Templates/Elements/lteButton.php
+++ Templates/Elements/lteButton.php
@@ -89,8 +89,8 @@ class lteButton extends lteAbstractElement
         /* @var $prefill_link \exface\Core\CommonLogic\WidgetLink */
         $prefill = '';
         if ($prefill_link = $this->getAction()->getPrefillWithDataFromWidgetLink()) {
-            if ($widget->getPage()->is($prefill_link->getPageAlias())) {
-                $prefill = ", prefill: " . $this->getTemplate()->getElement($prefill_link->getWidget())->buildJsDataGetter($this->getAction());
+            if ($prefill_link->getTargetPageAlias() === null || $widget->getPage()->is($prefill_link->getPageAlias())) {
+                $prefill = ", prefill: " . $this->getTemplate()->getElement($prefill_link->getTargetWidget())->buildJsDataGetter($this->getAction());
             }
         }
         
