16f3d3f6cb64a955a7e885e8bdf277bed60c4bf3
rml <rml@NBDR200.salt-solutions.de>
2019-05-28 18:39:01 +0200

    REFACTOR now using EChartsTrait

--- Facades/Elements/LteChart.php
+++ Facades/Elements/LteChart.php
@@ -3,7 +3,7 @@ namespace exface\AdminLTEFacade\Facades\Elements;
 
 use exface\Core\Facades\AbstractAjaxFacade\Elements\JqueryToolbarsTrait;
 use exface\Core\CommonLogic\Constants\Icons;
-use exface\Core\Facades\AbstractAjaxFacade\Elements\JqueryFlotTrait;
+use  exface\Core\Facades\AbstractAjaxFacade\Elements\EChartsTrait;
 use exface\Core\Facades\AbstractAjaxFacade\Elements\JqueryDataTableTrait;
 use exface\Core\DataTypes\BooleanDataType;
 
@@ -11,7 +11,7 @@ class LteChart extends lteDataTable
 {
     use JqueryToolbarsTrait;
     
-    use JqueryFlotTrait;
+    use EChartsTrait;
 
     public function init()
     {
@@ -62,7 +62,7 @@ class LteChart extends lteDataTable
             {$header}
         </div><!-- /.box-header -->
         <div class="box-body">
-            <div id="{$this->getId()}" style="{$style} width: calc(100% + 8px)"></div>
+            {$this->buildHtmlChart($style)}
         </div>
     </div>
     {$this->buildHtmlChartCustomizer()}
@@ -79,8 +79,6 @@ HTML;
         $widget = $this->getWidget();
         $output = '';
         
-        $output .= $this->buildJsFunctions();
-        
         // Add JS code for the configurator
         $output .= $this->getFacade()->getElement($widget->getConfiguratorWidget())->buildJs();
         // Add JS for all buttons
@@ -95,6 +93,11 @@ HTML;
     });
 JS;
         
+        $output .= $this->buildJsEChartsInit('light');
+        $output .= $this->buildJsFunctions();
+        $output .= $this->buildJsOnClickHandlers();
+        $output .= $this->buildJsRefresh();
+        
         return $output;
     }
     
@@ -106,7 +109,11 @@ JS;
         									, borderWidth: 1
         									, tickColor: "#f3f3f3"';
     }
-
+    
+    protected function buildJsDataLoadFunctionBody() : string
+    {
+        return $this->buildJsDataLoader();
+    }
     /**
      * Returns the JS code to fetch data: either via AJAX or from a Data widget (if the chart is bound to another data widget).
      *
@@ -150,7 +157,7 @@ JS;
                         method: "POST",
 						data: data,
 						success: function(data){
-							' . $this->buildJsRedraw('data') . ';
+							' . $this->buildJsRedraw('data.data') . ';
 							' . $this->buildJsBusyIconHide() . ';
 						},
 						error: function(jqXHR, textStatus, errorThrown){
@@ -291,7 +298,13 @@ HTML;
      */
     public function buildHtmlHeadTags()
     {
-        return $this->buildHtmlHeadDefaultIncludes();
+        $includes = $this->buildHtmlHeadDefaultIncludes();
+        // Resize-Sensor
+        $includes[] = '<script src="exface/vendor/npm-asset/css-element-queries/src/ResizeSensor.js"></script>';
+        $includes[] = '<link href="exface/vendor/bower-asset/magicsuggest/magicsuggest-min.css" rel="stylesheet">';
+        $includes[] = '<script src="exface/vendor/bower-asset/magicsuggest/magicsuggest-min.js"></script>';
+        
+        return $includes;
     }
     
     protected function hasBoxTitle() : bool
