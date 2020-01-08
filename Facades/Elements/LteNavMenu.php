75022f0dc2f351f2689be91f78aede10587e582e
rml <rml@NBDR326.salt-solutions.de>
2020-01-08 13:39:04 +0100

    NEW facade implementation for NavMenu widget

---
+++ Facades/Elements/LteNavMenu.php
@@ -0,0 +1,75 @@
+<?php
+namespace exface\AdminLTEFacade\Facades\Elements;
+
+use exface\Core\Widgets\NavMenu;
+
+/**
+ *
+ * @method NavMenu getWidget()
+ *
+ * @author Andrej Kabachnik
+ *
+ */
+class LteNavMenu extends LteAbstractElement
+{
+    public function buildHtml()
+    {
+        $menu = $this->getWidget()->getMenu();
+        return $this->buildHtmlMenu($menu);
+    }
+    
+    protected function buildHtmlMenu(array $menu, int $level = 1) : string
+    {
+        //TODO get the link prefix via a function, its hardcoded right now for testing
+        
+        if ($level === 1) {
+            $output = "<ul class='sidebar-menu'>";
+            $output .= "<li class='header'>MAIN NAVIGATION</li>";
+        } else {
+            $output = "<ul class ='treeview-menu'>";
+        }
+        foreach ($menu as $node) {
+            $url = $this->getFacade()->buildUrlToPage($node->getPageAlias());
+            if ($node->hasChildNodes()) {
+                if ($node->isInPath()) {
+                    $output .= <<<HTML
+                <li class='level{$level} treeview active'>
+                    <a href='{$url}'>{$node->getName()}</a>
+{$this->buildHtmlMenu($node->getChildNodes(), $level+1)}
+                </li>
+                
+                
+HTML;
+                } else {
+                    $output .= <<<HTML
+                <li class='level{$level} treeview'>
+                    <a href='{$url}'>{$node->getName()}</a>
+{$this->buildHtmlMenu($node->getChildNodes(), $level+1)}
+                </li>
+                
+HTML;
+
+                }
+            } elseif ($node->isInPath()) {
+                $output .= <<<HTML
+
+                <li class='level{$level} active current'>
+                    <a href='{$url}'>{$node->getName()}</a>
+                </li>
+                
+HTML;
+            } else {
+                $output .= <<<HTML
+                
+                <li class='level{$level}'>
+                    <a href='{$url}'>{$node->getName()}</a>
+                </li>
+                
+HTML;
+            }
+        }
+        $output .= "</ul>";
+        return $output;
+    }
+    
+}
