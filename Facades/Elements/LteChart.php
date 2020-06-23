1fcb2d999d5dd0cb862a96cb90a08d99902cd3ba
Andrej Kabachnik <andrej_ka@gmx.de>
2020-06-23 09:18:38 +0200

    FIX typo in lib include

--- Facades/Elements/LteChart.php
+++ Facades/Elements/LteChart.php
@@ -293,7 +293,7 @@ HTML;
         $includes = $this->buildHtmlHeadDefaultIncludes();
         $f = $this->getFacade();
         // Resize-Sensor
-        $includes[] = '<script src="' . $f->buildUrlToSource('LIBS.RESIZESINSOR.JS') . '"></script>';
+        $includes[] = '<script src="' . $f->buildUrlToSource('LIBS.RESIZESENSOR.JS') . '"></script>';
         $includes[] = '<link href="' . $f->buildUrlToSource('LIBS.MAGICSUGGEST.CSS') . '" rel="stylesheet">';
         $includes[] = '<script src="' . $f->buildUrlToSource('LIBS.MAGICSUGGEST.JS') . '"></script>';
         
